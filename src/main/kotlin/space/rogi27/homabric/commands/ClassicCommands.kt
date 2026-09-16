bd41fe05f3da7019f37e83911aa3252b79b590e5
27rogi <27rogi@pm.me>
2026-09-16 23:06:06 +0300

    chore: use PermissionLevel instead of numbers

--- src/main/kotlin/space/rogi27/homabric/commands/ClassicCommands.kt
+++ src/main/kotlin/space/rogi27/homabric/commands/ClassicCommands.kt
@@ -5,6 +5,7 @@ import me.lucko.fabric.api.permissions.v0.Permissions
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.commands.Commands
 import net.minecraft.commands.arguments.EntityArgument
+import net.minecraft.server.permissions.PermissionLevel
 import space.rogi27.homabric.Homabric
 import space.rogi27.homabric.config.HomabricConfig
 import space.rogi27.homabric.helpers.Completables
@@ -14,34 +15,34 @@ object ClassicCommands {
         if (HomabricConfig.areClassicCommandsEnabled()) {
             CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher, _, _ ->
                 dispatcher.register(
-                    Commands.literal("sethome").requires(Permissions.require("homabric.base.set", 0)).then(
+                    Commands.literal("sethome").requires(Permissions.require("homabric.base.set", PermissionLevel.ALL)).then(
                                 Commands.argument("home", StringArgumentType.word()).executes((BaseCommands::set))
                             ).executes(BaseCommands::set)
                 )
                 dispatcher.register(
-                    Commands.literal("removehome").requires(Permissions.require("homabric.base.remove", 0)).then(
+                    Commands.literal("removehome").requires(Permissions.require("homabric.base.remove", PermissionLevel.ALL)).then(
                         Commands.argument("home", StringArgumentType.word()).suggests(Completables::suggestPlayerHomes).executes((BaseCommands::remove))
                     )
                 )
                 dispatcher.register(
-                    Commands.literal("playerhome").requires(Permissions.require("homabric.base.others", 0)).then(
+                    Commands.literal("playerhome").requires(Permissions.require("homabric.base.others", PermissionLevel.ALL)).then(
                         Commands.argument("player", EntityArgument.player()).then(
                             Commands.argument("home", StringArgumentType.word()).suggests(Completables::suggestAllowedHomes).executes((BaseCommands::teleportToAllowed))
                         )
                     )
                 )
                 dispatcher.register(
-                    Commands.literal("homes").requires(Permissions.require("homabric.base.list", 0)).executes((BaseCommands::list))
+                    Commands.literal("homes").requires(Permissions.require("homabric.base.list", PermissionLevel.ALL)).executes((BaseCommands::list))
                 )
                 dispatcher.register(
-                    Commands.literal("allowhome").requires(Permissions.require("homabric.base.allow", 0)).then(
+                    Commands.literal("allowhome").requires(Permissions.require("homabric.base.allow", PermissionLevel.ALL)).then(
                                 Commands.argument("player", EntityArgument.player()).then(
                                             Commands.argument("home", StringArgumentType.word()).suggests(Completables::suggestPlayerHomes).executes((BaseCommands::allowHome))
                                         )
                             )
                 )
                 dispatcher.register(
-                    Commands.literal("disallowhome").requires(Permissions.require("homabric.base.disallow", 0)).then(
+                    Commands.literal("disallowhome").requires(Permissions.require("homabric.base.disallow", PermissionLevel.ALL)).then(
                         Commands.argument("player", EntityArgument.player()).then(
                                     Commands.argument("home", StringArgumentType.word()).suggests(Completables::suggestPlayerHomes).executes((BaseCommands::disallowHome))
                                 )
