0a18daf49837a6cbd4dbec9ef465fa554527d73b
27rogi <27rogi@pm.me>
2026-09-20 01:43:05 +0300

    chore: split config manager and class

--- src/main/kotlin/space/rogi27/homabric/config/ConfigManager.kt
+++ src/main/kotlin/space/rogi27/homabric/config/ConfigManager.kt
@@ -1,40 +1,7 @@
 package space.rogi27.homabric.config
 
 import net.fabricmc.loader.api.FabricLoader
-import org.spongepowered.configurate.hocon.HoconConfigurationLoader
-import space.rogi27.homabric.Homabric
 import java.nio.file.Files
-
-abstract class ConfigFile<T : Any>(
-    fileName: String,
-    private val type: Class<T>,
-    private val default: () -> T,
-) {
-    private val loader = HoconConfigurationLoader.builder()
-        .path(ConfigManager.resolve(fileName))
-        .build()
-
-    lateinit var entries: T
-        private set
-
-    init {
-        @Suppress("LeakingThis")
-        Homabric.logger.info("Loading config file $fileName")
-        ConfigManager.register(this)
-    }
-
-    fun load() {
-        val node = loader.load()
-        entries = node.get(type) ?: default()
-        save()
-    }
-
-    fun save() {
-        val node = loader.createNode()
-        node.set(type, entries)
-        loader.save(node)
-    }
-}
 
 object ConfigManager {
     private val configDir = FabricLoader.getInstance().configDir.resolve("homabric").also {
