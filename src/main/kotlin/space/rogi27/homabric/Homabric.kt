b0ac78afe555aedfc56ca8a8310a698ed394e439
27rogi <27rogi@pm.me>
2026-09-19 04:32:31 +0300

    feat: check config version on start

--- src/main/kotlin/space/rogi27/homabric/Homabric.kt
+++ src/main/kotlin/space/rogi27/homabric/Homabric.kt
@@ -6,13 +6,18 @@ import org.apache.logging.log4j.Logger
 import space.rogi27.homabric.commands.AdminCommands
 import space.rogi27.homabric.commands.BaseCommands
 import space.rogi27.homabric.commands.ClassicCommands
-import space.rogi27.homabric.config.ConfigManager
 import space.rogi27.homabric.config.HomabricConfig
 import space.rogi27.homabric.config.HomesConfig
 
 class Homabric:ModInitializer {
     override fun onInitialize() {
         config.load()
+
+        // TODO: find a way to dynamically check version
+        if(HomabricConfig.entries.configVersion != 3) {
+            throw Exception("Old config version detected! Please remove .minecraft/config/homabric to avoid errors!")
+        }
+
         homesConfig.load()
 
         BaseCommands.init()
